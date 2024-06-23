ce137ba715cab768d5d20e225297bd3faad4f3d7
Nikhil Bhaladhare <nikbhaladhare2104@gmail.com>
2024-06-23 22:53:31 +0530

    modify sign up page

--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -82,7 +82,7 @@ const SignUp = () => {
             <span>Add an avatar</span>
           </label>
           <button disabled={loading}>Sign up</button>
-          {loading && "Uploading and compressing the image please wait..."}
+          {loading && "Uploading the image please wait..."}
           {err && <span>Something went wrong</span>}
         </form>
         <p>
